7db282336ebfd35907568ab8c7c92faac2e47799
ajayrajaloor <ajayrajaloor@gmail.com>
2024-07-02 10:22:41 +0530

    updateCurrentUser in route and controller added and also added jwtparse to authorize the token and checked

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -24,32 +24,32 @@ try {
 }
 
 
-// const updateCurrentUser = async (req:Request,res:Response) => {
-//     try {
-//         const {name, addressLine1, country, city} = req.body
-//         const user = await User.findById(req.userId)
+const updateCurrentUser = async (req:Request,res:Response) => {
+    try {
+        const {name, addressLine1, country, city} = req.body
+        const user = await User.findById(req.userId)
 
-//         if(!user){
-//             res.status(404).json({message:"User not found"})
-//         }
+        if(!user){
+           return res.status(404).json({message:"User not found"})
+        }
 
-//         user.name = name;
-//         user.addressLine1 = addressLine1;
-//         user.country = country;
-//         user.city = city;
+        user.name = name;
+        user.addressLine1 = addressLine1;
+        user.country = country;
+        user.city = city;
 
-//         await user.save()
+        await user.save()
 
-//         res.send(user)
+        res.send(user)
 
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({message:"Error updating user"})
-//     }
-// }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Error updating user"})
+    }
+}
 
 
 export default {
     createCurrentUser,
-    // updateCurrentUser
+    updateCurrentUser
 }
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = express.Router()
 
@@ -8,7 +8,7 @@ const router = express.Router()
 
 // api/my-user
 router.post("/", jwtCheck, MyUserController.createCurrentUser)
-// router.put("/", MyUserController.updateCurrentUser)
+router.put("/", jwtCheck, jwtParse ,MyUserController.updateCurrentUser)
 
 
 export default router
