0270a0c1661fed5d4c96c56beb52ec16e690512c
ajayrajaloor <ajayrajaloor@gmail.com>
2024-07-02 11:39:08 +0530

    validation complete for the user requests

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router()
 
@@ -8,7 +9,7 @@ const router = express.Router()
 
 // api/my-user
 router.post("/", jwtCheck, MyUserController.createCurrentUser)
-router.put("/", jwtCheck, jwtParse ,MyUserController.updateCurrentUser)
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser)
 
 
 export default router
